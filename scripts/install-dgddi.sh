7d8c71c215ee52c7a590c7f59009f09255a6c88e
Winde4th <manuel.grosrenaud@gmail.com>
2016-09-07 15:21:38 +0200

    updated install script

--- scripts/install-dgddi.sh
+++ scripts/install-dgddi.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+﻿#!/bin/sh
 
 # Proceed post-deploy (backups)
-../bin/drush si --account-name=admin --account-pass=admin -y
+../bin/drush si --account-name=admin --account-pass=admin --site-name="Gestion de contenu DGDDI" --site-mail="do-not-replay@dgddi.fr" --locale=fr -y
 ../bin/drupal module:install dgddi_deploy -y
