e70d3b474c5a6c1aca73a41e5bd26ec176f60414
Winde4th <manuel.grosrenaud@gmail.com>
2016-09-07 15:30:03 +0200

    added drush cr

--- scripts/install-dgddi.sh
+++ scripts/install-dgddi.sh
@@ -2,4 +2,5 @@
 
 # Proceed post-deploy (backups)
 ../bin/drush si --account-name=admin --account-pass=admin --site-name="Gestion de contenu DGDDI" --site-mail="do-not-replay@dgddi.fr" --locale=fr -y
+../bin/drush cr
 ../bin/drupal module:install dgddi_deploy -y
